5543d06cee213f02063e582649447085a58269f3
Alexsandro Andrade <alexandrade.1979@gmail.com>
2020-08-01 16:31:18 -0300

    properties.json and housing.service created

--- Frontend/Houzze-Spa/src/app/property/property-list/property-list.component.ts
+++ Frontend/Houzze-Spa/src/app/property/property-list/property-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HousingService } from './../../services/housing.service';
 
 @Component({
   selector: 'app-property-list',
@@ -7,53 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyListComponent implements OnInit {
 
-  constructor() { }
+  properties: Array<any>;
+
+  constructor(private housingService: HousingService) { }
 
   ngOnInit(): void {
+    this.housingService.getAllProperties().subscribe(
+      data => {
+        this.properties = data;
+        console.log(data);
+      },
+      error => {
+        console.log('httperror: ');
+        console.log(error);
+      }
+    );
   }
 
-  Properties: Array<any> = [
-    {
-      "Id": 1,
-      "Price": 285000,
-      "Description": "Casa em condomínio fechado, 3 suítes, garagem coberta para 3 carros, ampla sala de estar, sala de jantar, lavanderia, lavabo, churrasqueira",
-      "Type": "Casa",
-      "Transaction": "Venda"
-    },
-    {
-      "Id": 2,
-      "Price": 2350,
-      "Description": "Casa em condomínio fechado, 3 suítes, garagem coberta para 3 carros, ampla sala de estar, sala de jantar, lavanderia, lavabo, churrasqueira",
-      "Type": "Apartamento",
-      "Transaction": "Aluguel"
-    },
-    {
-      "Id": 3,
-      "Price": 350000,
-      "Description": "Casa em condomínio fechado, 3 suítes, garagem coberta para 3 carros, ampla sala de estar, sala de jantar, lavanderia, lavabo, churrasqueira",
-      "Type": "Casa",
-      "Transaction": "Venda"
-    },
-    {
-      "Id": 4,
-      "Price": 2350,
-      "Description": "Casa em condomínio fechado, 3 suítes, garagem coberta para 3 carros, ampla sala de estar, sala de jantar, lavanderia, lavabo, churrasqueira",
-      "Type": "Casa",
-      "Transaction": "Aluguel"
-    },
-    {
-      "Id": 5,
-      "Price": 245000,
-      "Description": "Lindo terreno plano 600m² em condomínio fechado com 3 lagos, segurança 24 horas, sistema integrado de câmeras de segurança",
-      "Type": "Terreno",
-      "Transaction": "Venda"
-    },
-    {
-      "Id": 6,
-      "Price": 810000,
-      "Description": "Cobertura 192m², edifício jardins do sul, 4 suítes, 4 vagas na garagem",
-      "Type": "Apartamento cobertura",
-      "Transaction": "Venda"
-    },
-  ]
 }
